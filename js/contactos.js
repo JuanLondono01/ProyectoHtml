940a16e4f6b899aec38e9c932d2ab182ad8171ef
Juan Esteban Londoño Uribe <jelu0204@gmail.com>
2024-09-21 10:21:10 -0500

    vaciar los inputs cuando se envia el form

--- js/contactos.js
+++ js/contactos.js
@@ -1,6 +1,14 @@
 const subBtn = document.getElementById('sub-btn');
 const form = document.getElementById('contactForm');
 
+const nombre = document.getElementById('nombre')
+const telefono = document.getElementById('telefono')
+const correo = document.getElementById('correo')
+const empresa = document.getElementById('empresa')
+const aunto = document.getElementById('asunto')
+const pregunta = document.getElementById('pregunta')
+
+
 subBtn.addEventListener('click', (event) => {
     event.preventDefault();
     Swal.fire({
@@ -9,6 +17,12 @@ subBtn.addEventListener('click', (event) => {
         timer: 1500,
         showConfirmButton: false
     });
-
+    nombre.value = ''
+    telefono.value = ''
+    correo.value = ''
+    empresa.value = ''
+    aunto.value = ''
+    pregunta.value = ''
+    
     form.submit(); 
 });
